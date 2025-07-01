f75b60e711a8909ee9c6d2f003c9ab482ea476e7
Hadi <969hadiahmed@gmail.com>
2025-07-01 19:06:38 +0530

    feat: signin form

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,97 @@
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Container,
+  Avatar,
+  Link,
+  Paper,
+} from '@mui/material';
+
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useForm } from 'react-hook-form';
+import api from '../utils/axiosInstace';
+
+
+const signIn = async (data: SignInData) => {
+  try {
+    const res = await api.post('/auth/token', data);
+    console.log(res.data);
+
+  } catch (error) {
+    console.log(error);
+  }
+  return;
+};
+
+type SignInData = {
+  username: string;
+  password: string;
+};
+
+const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInData>();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} sx={{ mt: 8, p: 4, borderRadius: 3 }}>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <Box component="form" onSubmit={handleSubmit(signIn)} sx={{ mt: 3 }}>
+            <TextField
+              fullWidth
+              label="Email Address or Username"
+              margin="normal"
+              {...register('username', {
+                required: 'Email is required',
+              })}
+              error={!!errors.username}
+              helperText={errors.username?.message}
+            />
+
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              margin="normal"
+              {...register('password', { required: 'Password is required' })}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 2 }}
+            >
+              Sign In
+            </Button>
+
+            <Box mt={2} display="flex" justifyContent="flex-end">
+              <Link href="/signup" variant="body2">
+                Don't have an account? Sign Up
+              </Link>
+            </Box>
+
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
+
+export default SignInForm;
+
