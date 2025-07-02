f3628dcef5f757a8e9f133d3b7311babd0fd2ae1
Hadi <969hadiahmed@gmail.com>
2025-07-02 14:00:04 +0530

    feat: component to show list of PlanCards

---
+++ src/components/PlanList.tsx
@@ -0,0 +1,21 @@
+// src/components/PlanList.tsx
+import {
+  Box,
+} from '@mui/material';
+import PlanCard from './PlanCard';
+import type { Plan } from '../types/plan.type';
+
+
+const PlanList = ({ plans }: { plans: Plan[] }) => {
+
+
+  return (
+    <Box display="grid" gap={2}>
+      {plans.map((plan) => (
+        <PlanCard key={plan.id} plan={plan} />
+      ))}
+    </Box>
+  );
+};
+
+export default PlanList;
