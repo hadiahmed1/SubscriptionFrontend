bef0183ba954f015e7f1e9b5dcb9af510f3473da
Hadi <969hadiahmed@gmail.com>
2025-07-02 15:11:59 +0530

    feat: featching Plans from backend

--- src/pages/PlansPage.tsx
+++ src/pages/PlansPage.tsx
@@ -1,5 +1,41 @@
-const PlansPage=()=>{
-    return(<>PlansPage</>)
+import { useEffect, useState } from "react";
+import PlanList from "../components/PlanList";
+import type { Plan } from "../types/plan.type";
+import { fetchAllPlans } from "../services/plan.service";
+import { CircularProgress } from "@mui/material";
+
+
+
+const PlansPage = () => {
+
+    const [plans, setPlans] = useState<Plan[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const getPlans = async () => {
+            setLoading(true)
+            try {
+                const data = await fetchAllPlans();
+                setPlans(data);
+            } catch (error) {
+                setError("Failed to fetch plans:" + error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getPlans();
+    }, []);
+    useEffect(() => {
+        console.log(error);
+
+    }, [error])
+
+    return (<>
+        {loading ? <CircularProgress size={40} /> : <PlanList plans={plans} />}
+
+    </>)
 }
 
 export default PlansPage;
