cd19d011d5de1c52e81f25dbe9c839482beb61d4
Hadi <969hadiahmed@gmail.com>
2025-07-02 08:59:29 +0530

    fix: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router"
 import PlansPage from "./pages/PlansPage"
-import AuthPage from "./pages/AuthPage"
 import CompanyPage from "./pages/CompanyPage"
 import MySubscribtionsPage from "./pages/MySubscribtionsPage"
 import NavBar from "./components/NavBar"
 import "./index.css"
+import SignInForm from "./components/SignInForm"
+import SignUpForm from "./components/SignUpForm"
 
 function App() {
 
@@ -13,7 +14,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route index element={<PlansPage />} />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="signin" element={<SignInForm />} />
+        <Route path="signup" element={<SignUpForm />} />
+        
         <Route path="/company" element={<CompanyPage />} />
         <Route path="/mysubscriptions" element={<MySubscribtionsPage />} />
       </Routes>
