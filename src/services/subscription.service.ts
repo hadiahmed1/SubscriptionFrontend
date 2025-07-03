9dd5dd1b290915d10213bb63a7707d92c2fad269
Hadi <969hadiahmed@gmail.com>
2025-07-03 11:44:44 +0530

    feat: fetchMySubscriptions function

--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import api from "../utils/axiosInstace";
+import type { Subscription } from "../types/subsctiption";
 
 export const suscribe = async (planId: string) => {
     try {
-        const res = await api.get<unknown[]>('/subscription');
+        const res = await api.post<unknown[]>(`/subscription/${planId}`);
         return res.data;
     } catch (error) {
         if (axios.isAxiosError(error))
@@ -13,3 +14,16 @@ export const suscribe = async (planId: string) => {
         throw error;
     }
 }
+
+export const fetchMySubscriptions = async (): Promise<Subscription[]> => {
+    try {
+        const res = await api.get<Subscription[]>('/subscription');
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error))
+            console.error("Axios error while fetching subscription:", error.response?.data || error.message);
+        else
+            console.error("Unexpected error while fetching subscription:", error);
+        throw error;
+    }
+};
