f04b2ba3fb24bd7f89038c735534e3bb774894b2
Hadi <969hadiahmed@gmail.com>
2025-07-03 19:29:54 +0530

    feat: CompanyPage Layout

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -1,11 +1,26 @@
+import { Box } from "@mui/material";
 import CompanyFeatureList from "../components/CompanyFeatureList";
 import CompanyPlanList from "../components/CompanyPlanList";
 
 const CompanyPage = () => {
-    return (<>
-        <CompanyPlanList />
-        <CompanyFeatureList />
-    </>)
+    return (
+        <>
+            <Box
+                sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+            >
+                <Box sx={{ flex: 3, overflow: "auto" }}>
+                    <CompanyPlanList />
+                </Box>
+                <Box sx={{ flex: 2, overflow: "auto", px: 3 }}>
+                    <CompanyFeatureList />
+                </Box>
+            </Box>
+        </>
+    );
 }
 
 export default CompanyPage;
