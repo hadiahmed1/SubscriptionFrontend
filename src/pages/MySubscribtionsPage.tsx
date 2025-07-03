9eb2ced3cbee47e6ab246eded7176dc5f2908c9c
Hadi <969hadiahmed@gmail.com>
2025-07-03 11:43:52 +0530

    feat:MySubscribtionsPage
    Shows all the plans a loggedin user is suscribed to

--- src/pages/MySubscribtionsPage.tsx
+++ src/pages/MySubscribtionsPage.tsx
@@ -1,113 +1,44 @@
-const subscription = [
-    {
-        "id": "dbdfcd58-a161-4d23-bf1d-ca38a18eb16e",
-        "subscriber": null,
-        "subscriberId": "9e28e8b5-5566-4e7e-9899-cb13f301cabf",
-        "plan": {
-            "id": "4cec630f-5751-4fb3-b333-564d064a99d5",
-            "company": null,
-            "companyId": "f0b7b111-ab43-4fce-8955-3a35de6dcbc4",
-            "name": "platinum plan",
-            "description": "plan with features",
-            "cost": 2000,
-            "discount": 0,
-            "subscriptions": null,
-            "features": [
-                {
-                    "planId": "4cec630f-5751-4fb3-b333-564d064a99d5",
-                    "featureId": "0238c29d-dbd9-4217-9dd7-2ed7460406a9",
-                    "plan": null,
-                    "feature": {
-                        "id": "0238c29d-dbd9-4217-9dd7-2ed7460406a9",
-                        "company": null,
-                        "companyId": "f0b7b111-ab43-4fce-8955-3a35de6dcbc4",
-                        "name": "f1",
-                        "description": "feature1",
-                        "plans": null
-                    }
-                },
-                {
-                    "planId": "4cec630f-5751-4fb3-b333-564d064a99d5",
-                    "featureId": "dd684835-bfcf-4620-8994-695b30263d87",
-                    "plan": null,
-                    "feature": {
-                        "id": "dd684835-bfcf-4620-8994-695b30263d87",
-                        "company": null,
-                        "companyId": "f0b7b111-ab43-4fce-8955-3a35de6dcbc4",
-                        "name": "f2",
-                        "description": "feature2",
-                        "plans": null
-                    }
-                }
-            ],
-            "created_at": "2025-06-30T13:00:35.790000Z",
-            "updated_at": "2025-06-30T13:00:35.790000Z",
-            "deleted_at": null
-        },
-        "planId": "4cec630f-5751-4fb3-b333-564d064a99d5",
-        "payments": null,
-        "expiresOn": "2025-07-31T03:42:09.383000Z",
-        "created_at": "2025-07-01T03:42:09.386000Z",
-        "updated_at": "2025-07-01T03:42:09.386000Z",
-        "deleted_at": null
-    },
-    {
-        "id": "a197e596-b79c-474e-b25d-5fa3001627fc",
-        "subscriber": null,
-        "subscriberId": "9e28e8b5-5566-4e7e-9899-cb13f301cabf",
-        "plan": {
-            "id": "4cec630f-5751-4fb3-b333-564d064a99d5",
-            "company": null,
-            "companyId": "f0b7b111-ab43-4fce-8955-3a35de6dcbc4",
-            "name": "platinum plan",
-            "description": "plan with features",
-            "cost": 2000,
-            "discount": 0,
-            "subscriptions": null,
-            "features": [
-                {
-                    "planId": "4cec630f-5751-4fb3-b333-564d064a99d5",
-                    "featureId": "0238c29d-dbd9-4217-9dd7-2ed7460406a9",
-                    "plan": null,
-                    "feature": {
-                        "id": "0238c29d-dbd9-4217-9dd7-2ed7460406a9",
-                        "company": null,
-                        "companyId": "f0b7b111-ab43-4fce-8955-3a35de6dcbc4",
-                        "name": "f1",
-                        "description": "feature1",
-                        "plans": null
-                    }
-                },
-                {
-                    "planId": "4cec630f-5751-4fb3-b333-564d064a99d5",
-                    "featureId": "dd684835-bfcf-4620-8994-695b30263d87",
-                    "plan": null,
-                    "feature": {
-                        "id": "dd684835-bfcf-4620-8994-695b30263d87",
-                        "company": null,
-                        "companyId": "f0b7b111-ab43-4fce-8955-3a35de6dcbc4",
-                        "name": "f2",
-                        "description": "feature2",
-                        "plans": null
-                    }
-                }
-            ],
-            "created_at": "2025-06-30T13:00:35.790000Z",
-            "updated_at": "2025-06-30T13:00:35.790000Z",
-            "deleted_at": null
-        },
-        "planId": "4cec630f-5751-4fb3-b333-564d064a99d5",
-        "payments": null,
-        "expiresOn": "2025-07-31T03:43:22.397000Z",
-        "created_at": "2025-07-01T03:43:22.400000Z",
-        "updated_at": "2025-07-01T03:43:22.400000Z",
-        "deleted_at": null
-    }
-]
+import { useState, useEffect } from "react";
+import PlanCard from "../components/PlanCard";
+import type { Subscription } from "../types/subsctiption";
+import { fetchMySubscriptions } from "../services/subscription.service";
+import { CircularProgress } from "@mui/material";
 
 
 const MySubscribtionsPage = () => {
-    return (<>MySubscribtionsPage</>)
+    const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const getSubscriptions = async () => {
+            setLoading(true)
+            try {
+                const data = await fetchMySubscriptions();
+                setSubscriptions(data);
+            } catch (error) {
+                setError("Failed to fetch plans:" + error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getSubscriptions();
+    }, []);
+    useEffect(() => {
+        console.log(error);
+
+    }, [error])
+
+    return (<>
+        {
+            loading ? <CircularProgress size={40} /> :
+                subscriptions.map(sub => <PlanCard key={sub.id} plan={sub.plan} expiresOn={sub.expiresOn} />)
+        }
+
+
+    </>)
 }
 
 export default MySubscribtionsPage;
+
